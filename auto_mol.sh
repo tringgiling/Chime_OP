2f20932a6bc52f1f12262b42daec5936f70e5a49
tringgiling <59884216+tringgiling@users.noreply.github.com>
2021-12-01 13:32:49 +0700

    sorted

--- auto_mol.sh
+++ auto_mol.sh
@@ -16,16 +16,16 @@ list_log=$(ls -- *[.log])
 for item in $list_log; do
     cat "hasil_MOL.txt"
     echo "$item"
-    cat "$item" | grep -oP "(?<=source: /opt/raw_data/)[^ ]*" | tr '\n' ',' >> "hasil_MOL.txt" #search nama OSS, tech nya + vendornya , dan trim newline menjadi "," supaya mudah dibuat csv file
-    cat "$item" | grep -oP "(?<=Finishing loading network model to DB.Number of records loaded )[^ ]*" >> "hasil_MOL.txt" #search nilai MOLoad di OSS tersebut
-    cp "hasil_MOL.txt" ..
+    grep -oP "(?<=source: /opt/raw_data/)[^ ]*" "$item" | tr '\n' ',' >> "hasil_MOL.txt" #search nama OSS, tech nya + vendornya , dan trim newline menjadi "," supaya mudah dibuat csv file
+    grep -oP "(?<=Finishing loading network model to DB.Number of records loaded )[^ ]*" "$item" >> "hasil_MOL.txt" || if [ $? -eq 1 ] ; then echo "fail" >> "hasil_MOL.txt" ; else return; fi #search nilai MOLoad di OSS tersebut
 done
+cp "hasil_MOL.txt" ..
 )
 
 ## mensortir file nya sesuai teknologi, yang 2g, 3g dan 4g
-(grep "2g" hasil_MOL.txt > MOL_2G.csv
-grep "3g" hasil_MOL.txt > MOL_3G.csv
-grep "4g" hasil_MOL.txt > MOL_4G.csv
+(grep "2g" hasil_MOL.txt | sort > MOL.csv ; echo " " >> MOL.csv
+grep "3g" hasil_MOL.txt  | sort >> MOL.csv ; echo " " >> MOL.csv
+grep "4g" hasil_MOL.txt  | sort >> MOL.csv
 )
 
 ##aktifitas selesai, saatnya bersih bersih
