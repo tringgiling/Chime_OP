21b1789d5673d1581b843544640225f383a7c2ce
tringgiling <59884216+tringgiling@users.noreply.github.com>
2021-12-02 09:25:48 +0700

    sudah lulus shellcheck

--- auto_mol.sh
+++ auto_mol.sh
@@ -14,7 +14,6 @@ gzip -d -- *.gz #buka bungkus log yang  berekstensi .gz
 (cd MOL/ || return #return buat antisipasi kalau gagal masuk folder
 list_log=$(ls -- *[.log])
 for item in $list_log; do
-    cat "hasil_MOL.txt"
     echo "$item"
     grep -oP "(?<=source: /opt/raw_data/)[^ ]*" "$item" | tr '\n' ',' >> "hasil_MOL.txt" #search nama OSS, tech nya + vendornya , dan trim newline menjadi "," supaya mudah dibuat csv file
     grep -oP "(?<=Finishing loading network model to DB.Number of records loaded )[^ ]*" "$item" >> "hasil_MOL.txt" || if [ $? -eq 1 ] ; then echo "fail" >> "hasil_MOL.txt" ; else return; fi #search nilai MOLoad di OSS tersebut
@@ -22,13 +21,13 @@ done
 cp "hasil_MOL.txt" ..
 )
 
-## mensortir file nya sesuai teknologi, yang 2g, 3g dan 4g
-(grep "2g" hasil_MOL.txt | sort > MOL.csv ; echo " " >> MOL.csv
-grep "3g" hasil_MOL.txt  | sort >> MOL.csv ; echo " " >> MOL.csv
-grep "4g" hasil_MOL.txt  | sort >> MOL.csv
-)
+## mensortir file nya sesuai teknologi dan diurut sesuai OSS, disimpan dalam bentuk file csv
+(grep "2g" hasil_MOL.txt | sort  ; echo " "
+grep "3g" hasil_MOL.txt  | sort  ; echo " "
+grep "4g" hasil_MOL.txt  | sort ) > MOL.csv
+
 
 ##aktifitas selesai, saatnya bersih bersih
-simpen_file=$(zenity --file-selection --directory --title="Pilih tempat simpan file" --filename=/home/iqbal/Kerja/imobi/task/Operation/log/)
+simpen_file=$(zenity --file-selection --directory --title="Pilih tempat simpan file" --filename="$pilih_file_MOL")
 mv MOL.csv hasil_MOL.txt "$simpen_file" #pindahin file csv ke folder pilihan pengguna
 rm --recursive MOL
--- auto_nl.sh
+++ auto_nl.sh
@@ -14,21 +14,19 @@ gzip -d -- *.gz #buka bungkus log yang  berekstensi .gz
 (cd NL/ || return #return buat antisipasi kalau gagal masuk folder
 list_log=$(ls -- *[.log])
 for item in $list_log; do
-    cat "hasil_NL.txt"
-    echo "$item"
+    echo "Sedang memproses $item"
     grep -oP "(?<=Path /opt/raw_data/)[^ ]*" "$item" | tr '\n' ',' >> "hasil_NL.txt" #search nama OSS, tech nya + vendornya , dan trim newline menjadi "," supaya mudah dibuat csv file
-    egrep -o "[0-9]{1,4} file-based networks were built" "$item" | egrep -o "[0-9]{1,4}" >> "hasil_NL.txt" || if [ $? -eq 1 ] ; then echo "fail" >> "hasil_NL.txt" ; else return; fi #search nilai MOLoad di OSS tersebut, ambil angkanya aja
+    egrep -o "[0-9]{1,4} file-based networks were built" "$item" | egrep -o "[0-9]{1,4}" >> "hasil_NL.txt" || if [ $? -eq 1 ] ; then echo "fail" >> "hasil_NL.txt" ; else return; fi #search nilai NLoad di OSS tersebut, ambil angkanya aja
 done
-cp "hasil_NL.txt" ..
-)
+echo "Selesai"
+cp "hasil_NL.txt" ..)
 
-## mensortir file nya sesuai teknologi, yang 2g, 3g dan 4g, diurutkan sesuai abjad OSS
-(grep "2g" hasil_NL.txt | sort > NL.csv ; echo " " >> NL.csv
-grep "3g" hasil_NL.txt  | sort >> NL.csv ; echo " " >> NL.csv
-grep "4g" hasil_NL.txt  | sort >> NL.csv
-)
+#Urutkan sesuai teknologi dan OSS, lalu simpan hasilnya dalam bentuk csv
+(grep "2g" hasil_NL.txt | sort ; echo " "
+grep "3g" hasil_NL.txt  | sort ; echo " "
+grep "4g" hasil_NL.txt  | sort ) >NL.csv
 
 ##aktifitas selesai, saatnya simpen file penting dan bersih bersih
-simpen_file=$(zenity --file-selection --directory --title="Pilih tempat simpan file" --filename=/home/iqbal/Kerja/imobi/task/Operation/log/)
+simpen_file=$(zenity --file-selection --directory --title="Pilih tempat simpan file" --filename="$pilih_file_NL")
 mv NL.csv hasil_NL.txt "$simpen_file" #pindahin file csv ke folder pilihan pengguna
 rm --recursive NL #bersihin folder tempat ekstrak file NL
